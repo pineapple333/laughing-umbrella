b8e6223522aabd7accc97e21cae76ba2e7b73eaa
pineapple333 <36419747+pineapple333@users.noreply.github.com>
2021-04-13 16:42:34 +0200

    Added 'Publication' data model

--- DisplayApp/models.py
+++ DisplayApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Publication(models.Model):
+    authors = models.CharField(max_length=1000)
+    title = models.CharField(max_length=256)
+    points = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.authors}; {self.title}; {self.points}'
--- SWEProject/urls.py
+++ SWEProject/urls.py
@@ -17,6 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('app/', include('DisplayApp.urls')),
     path('admin/', admin.site.urls),
 ]
