2d0da8ffcd877eb4ed9768011205d3810d051c83
mwalkowicz <michal.walkowicz@student.uj.edu.pl>
2021-05-07 18:10:52 +0200

    Added calculation of points ands costs

--- DisplayApp/models.py
+++ DisplayApp/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Publication(models.Model):
     authors = models.CharField(max_length=1000)
     title = models.CharField(max_length=256)
-    points = models.IntegerField(default=0)
+    points = models.IntegerField(default=5)
+	cost = models.FloatField(default=0)
 
     def __str__(self):
         return f'{self.authors}; {self.title}; {self.points}'
--- Search/models.py
+++ Search/models.py
@@ -3,11 +3,13 @@ from django.db import models
 
 # Create your models here.
 class Publication:
-    def __init__(self):
-        self.authors = []
-        self.title = ""
-        self.points = 0
-        self.year = 0
+	def __init__(self):
+		self.authors = []
+		self.title = ""
+		self.points = 0
+		self.year = 0
+		self.cost = 0
+		self.m = 0
 
-    def __repr__(self):
-        return f'\n\tAuthors: {self.authors},\n\tTitle: {self.title},\n\tPoints: {self.points},\n\tYear: {self.year}'
+	def __repr__(self):
+		return f'\n\tAuthors: {self.authors},\n\tTitle: {self.title},\n\tPoints: {self.points},\n\tYear: {self.year}'
